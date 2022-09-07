974fd05bfaf1c62023db68b53625730190203576
napat103 <naphat.watt@kmutt.ac.th>
2022-09-08 00:36:12 +0700

    finish

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return weight/(height*height)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  if(calculateBMI(weight, height) < 18.5) {
+    return "Underweight"
+  } else if(18.5 < calculateBMI(weight, height) && calculateBMI(weight,height) < 24.9) {
+    return "Normal weight"
+  } else if(25 < calculateBMI(weight, height)) {
+    return "Overweight"
+  }
 }
-module.exports = getBMIMeaning
+// module.exports = getBMIMeaning
+
+console.log(getBMIMeaning(65, 1.8))
+console.log(getBMIMeaning(100, 1.7))
+console.log(getBMIMeaning(44, 1.6))
